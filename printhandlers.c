3a735e660fcb2f5b3a8a32625f9eaf86edc28a7e
dev_EstherKubania <eskury@gmail.com>
2023-05-22 11:00:40 -0700

    handle output

---
+++ printhandlers.c
@@ -0,0 +1,70 @@
+#include "main.h"
+/**
+ * _fputs - Custom implementation of fputs function
+ * @str: String to be printed
+ * @stream: Pointer to the output stream
+ * Return: Number of characters printed
+ */
+int _fputs(const char *str, FILE *stream)
+{
+	int i, count = 0;
+
+	if (str == NULL)
+	return (-1);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	fputc(str[i], stream);
+	count++;
+	}
+
+	return (count);
+}
+/**
+ * _puts - Custom implementation of puts function
+ * @str: String to be printed
+ * Return: Number of characters printed
+ */
+int _puts(char *str)
+{
+	int i, count = 0;
+
+	if (str == NULL)
+	return (-1);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	_putchar(str[i]);
+	count++;
+	}
+
+	return (count);
+}
+/**
+ * _getenv - Retrieves the value of the environment variable.
+ * @name: The name of the environment variable.
+ *
+ * Return: On success, returns a pointer to the value associated with the
+ *	environment variable name. Otherwise, returns NULL.
+ */
+char *_getenv(const char *name)
+{
+	char *value = NULL;
+	size_t name_length = strlen(name);
+
+	if (name == NULL || name_length == 0)
+	return (NULL);
+
+	for (int i = 0; environ[i] != NULL; i++)
+	{
+	if (strncmp(name, environ[i], name_length) == 0
+		&& environ[i][name_length] == '=')
+	{
+	value = environ[i] + name_length + 1;
+	break;
+	}
+	}
+
+	return (value);
+}
+
