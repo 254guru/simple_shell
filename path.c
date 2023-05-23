f1dcc55de17857abfcbab49d876b7cb00b7552a2
dev_EstherKubania <eskury@gmail.com>
2023-05-23 08:58:52 -0700

    handle path

--- path.c
+++ path.c
@@ -1,82 +1,84 @@
 #include "main.h"
+
 #define MAX_COMMAND_LENGTH 100
 #define MAX_PATH_LENGTH 200
 
 /**
- * get_command_path - gets the full path of a command using PATH variable
- * @command: command to find
- *
- * Return: full path of the command, otherwise NULL
+ * get_command_path - Get the full path of a command using the PATH variable
+ * @command: Command to find
+ * Return: Full path of the command if found, NULL otherwise
  */
-
 char *get_command_path(char *command)
 {
-	char *path;
-	char *dir;
-	char *full_path;
-	char *token;
+    char *path;
+    char *dir;
+    char *full_path;
+    char *token;
 
-	if (_strchr(command, '/') != NULL)
-	{
-		return (_strdup(command));
-	}
-	path = getenv("PATH");
+    if (strchr(command, '/') != NULL)
+    {
+        return strdup(command);
+    }
 
-	if (path == NULL)
-	{
-		fprintf(stderr, "Error retrieving PATH variable\n");
-		return (NULL);
-	}
-	token = strtok(path, ":");
-	while (token != NULL)
-	{
-		dir = _strdup(token);
-		full_path = malloc(_strlen(dir) + _strlen(command) + 2);
+    path = getenv("PATH");
 
-		if (full_path == NULL)
-		{
-			fprintf(stderr, "Allocation error\n");
-			free(dir);
-			return (NULL);
-		}
-		sprintf(full_path, "%s/%s", dir, command);
+    if (path == NULL)
+    {
+        write(STDERR_FILENO, "Error retrieving PATH variable\n", 31);
+        return NULL;
+    }
 
-		if (access(full_path, X_OK) == 0)
-		{
-			free(dir);
-			return (full_path);
-		}
-		free(full_path);
-		free(dir);
-		token = strtok(NULL, ":");
-	}
-	return (NULL);
+    token = strtok(path, ":");
+    while (token != NULL)
+    {
+        dir = strdup(token);
+        full_path = malloc(_strlen(dir) + _strlen(command) + 2);
+        if (full_path == NULL)
+        {
+            write(STDERR_FILENO, "Allocation error\n", 17);
+            free(dir);
+            return NULL;
+        }
+       _strcpy(full_path, dir);
+       strcat(full_path, "/");
+       strcat(full_path, command);
+
+        if (access(full_path, X_OK) == 0)
+        {
+            free(dir);
+            return full_path;
+        }
+
+        free(full_path);
+        free(dir);
+        token = strtok(NULL, ":");
+    }
+
+    return NULL;
 }
 /**
- * build_command_path - BUILD THE FULL PATH OF A COMMAND
- * @directory: directory to prepend the command
- * @command: command to append to the directory
- *
- * Return: full path of the command
+ * build_command_path - Build the full path of a command
+ * @directory: Directory to prepend to the command
+ * @command: Command to append to the directory
+ * Return: Full path of the command
  */
 char *build_command_path(char *directory, char *command)
 {
-	size_t directory_len = _strlen(directory);
-	size_t command_len = _strlen(command);
-	size_t path_len = directory_len + command_len + 2;
-	char *path;
+    size_t directory_len = _strlen(directory);
+    size_t command_len = _strlen(command);
+    size_t path_len = directory_len + command_len + 2;
 
-	path = malloc(path_len);
-	if (path == NULL)
-	{
-		fprintf(stderr, "Allocation error\n");
-		exit(EXIT_FAILURE);
-	}
+    char *path = malloc(path_len);
+    if (path == NULL)
+    {
+        write(STDERR_FILENO, "Allocation error\n", 17);
+        exit(EXIT_FAILURE);
+    }
 
-	_strncpy(path, directory, directory_len);
-	path[directory_len] = '/';
-	_strncpy(path + directory_len + 1, command, command_len);
-	path[path_len - 1] = '\0';
+    strncpy(path, directory, directory_len);
+    path[directory_len] = '/';
+    strncpy(path + directory_len + 1, command, command_len);
+    path[path_len - 1] = '\0';
 
-	return (path);
+    return path;
 }
