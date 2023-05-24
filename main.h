ceeceab6552fb60c62de00f354163d160922ed60
dev_EstherKubania <eskury@gmail.com>
2023-05-24 02:01:40 -0700

    updates

--- main.h
+++ main.h
@@ -48,13 +48,10 @@ int cd(char *directory);
 int myunsetenv(char *variable);
 int mysetenv(char *variable, char *value, int overwrite);
 int handle_semicolon(char *line);
-<<<<<<< HEAD
 void non_interactive_mode(char *filename);
 void interactive_mode();
 void process_command(char *command, int counter);
-=======
 int _putenv(char *string);
->>>>>>> 4a6289a460f2da4587bb123ae467b7eb1598be37
 /* helper function prototypes */
 int _atoi(const char *str);
 void free_args(char **args);
