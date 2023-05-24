f7241060ba58978938e9bb05c60ac7b257bee66e
dev-EstherKubania <eskury@gmail.com>
2023-05-24 02:46:17 -0700

    cd builtin

--- cd.c
+++ cd.c
@@ -1,28 +1,37 @@
 #include "main.h"
 
-int cd(char *directory) {
-  char *cwd = getcwd(NULL, 0);
-  int rc = chdir(directory);
-	if (directory == NULL) {
-    directory = getenv("HOME");
-  }
+/**
+ * cd - Changes the current directory.
+ * @directory: The directory path to change to.
+ * Return: On success, 0. On error, 1.
+ */
+int cd(char *directory)
+{
+        char *cwd = getcwd(NULL, 0);
+        int rc = chdir(directory);
 
-  if (directory == NULL) {
-    fprintf(stderr, "cd: directory '%s' does not exist\n", directory);
-    return 1;
-  }
+        if (directory == NULL)
+        directory = _getenv("HOME");
 
-  if (rc != 0) {
-    fprintf(stderr, "cd: cannot change directory to '%s'\n", directory);
-    return 1;
-  }
+        if (directory == NULL)
+        {
+        _fputs("cd: directory does not exist\n", stderr);
+        return (1);
+        }
 
-  if (cwd == NULL) {
-    fprintf(stderr, "cd: cannot get current working directory\n");
-    return 1;
-  }
-  mysetenv("PWD", cwd, 1);
+        if (rc != 0)
+        {
+        _fputs("cd: cannot change directory\n", stderr);
+        return (1);
+        }
 
-  return 0;
+        if (cwd == NULL)
+        {
+        _fputs("cd: cannot get current working directory\n", stderr);
+        return (1);
+        }
+
+        mysetenv("PWD", cwd, 1);
+
+        return (0);
 }
-
