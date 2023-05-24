031049707ddb4521d223af8d159449450f32b28d
dev-EstherKubania <eskury@gmail.com>
2023-05-24 03:42:35 -0700

    main function

--- main.c
+++ main.c
@@ -1,50 +1,74 @@
 #include "main.h"
+#include <signal.h>
+
+volatile sig_atomic_t flag = 0;
+
+void handle_signal(int signal)
+{
+    if (signal == SIGINT)
+    {
+        flag = (1);
+    }
+}
 
 /*
  * print_prompt - Prints a prompt to the stdout stream
- * Return: void
+ * Return: 0
  */
-void print_prompt(void)
+int print_prompt(void)
 {
-	char *prompt = "($) ";
+    char *prompt = "($) ";
 
-	write(STDOUT_FILENO, prompt, _strlen(prompt));
+    ssize_t bytesWritten = write(STDOUT_FILENO, prompt, strlen(prompt));
+    if (bytesWritten == -1) {
+        perror("write");
+        exit(EXIT_FAILURE);
+    }
 }
 /**
- * prompt - Reads a line of input from the user.
- * @line: Pointer to the variable where the input line will be stored.
- * Return: 1 if a line of input was successfully read.
- *	0 if the input line is empty or end of input is reached.
- *	-1 if an error occurred while reading input.
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ * @envp: Array of environment variables
+ * Return: Always 0 (success)
  */
 int prompt(char **line)
 {
-	size_t bufferSize = 0;
-	ssize_t bytesRead;
-	char *newLine = NULL;
+    size_t bufferSize = 0;
+    ssize_t bytesRead;
+    char *newLine = NULL;
 
-	bytesRead = getline(&newLine, &bufferSize, stdin);
+    bytesRead = getline(&newLine, &bufferSize, stdin);
 
-	if (bytesRead == -1)
-	{
-	free(newLine);
-	return (-1);
-	}
-	else if (bytesRead == 0)
-	{
-	free(newLine);
-	return (0);
-	}
-	else
-	{
-	if (newLine[bytesRead - 1] == '\n')
-	{
-	newLine[bytesRead - 1] = '\0';
-	}
-	free(*line);
-	*line = newLine;
-	return (1);
-	}
+    if (bytesRead == -1)
+    {
+        if (flag)
+        {
+            free(newLine);
+            return (-1);
+        }
+        else
+        {
+            perror("getline");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else if (bytesRead == 0)
+    {
+        free(newLine);
+        return (0);
+    }
+    else
+    {
+        if (newLine[bytesRead - 1] == '\n')
+        {
+            newLine[bytesRead - 1] = '\0';
+        }
+
+        free(*line);
+        *line = newLine;
+        return (1);
+    }
 }
 /**
  * main - Entry point of the program
@@ -55,52 +79,60 @@ int prompt(char **line)
  */
 int main(int argc, char **argv, char **envp)
 {
-	char *line = NULL, **args;
-	int exit_status;
-	int status;
-	(void)argc;
-	(void)envp;
-	program_name = argv[0];
+    char *line = NULL;
+    char **args;
+    int status;
+    (void)argc;
+    (void)envp;
+    program_name = argv[0];
+    signal(SIGINT, handle_signal);
 
-	while (1)
-	{
-		print_prompt();
-		if (prompt(&line) == -1)
-		{
-			exit(0);
-		}
-		args = split_line(line);
-		if (args == NULL)
-			continue;
-		if (args != NULL && *args != NULL)
-		{
-			if (_strcmp(args[0], "exit") == 0)
-			{
-				if (args[1] != NULL)
-				{
-					exit_status = _atoi(args[1]);
+    while (1)
+    {
+        print_prompt();
 
-					free(line);
-					free_args(args);
-					exit(exit_status);
-				}
-				else
-				{
-					free(line);
-					free_args(args);
-					exit(0);
-				}
-			}
-			status = execute(args);
-			free_args(args);
-		}
+        if (prompt(&line) == -1)
+        {
+            free(line);
+            exit(EXIT_SUCCESS);
+        }
+        args = split_line(line);
+        if (args == NULL)
+        {
+            continue;
+        }
 
-		free(line);
-		line = NULL;
-		if (status == EXIT_SUCCESS)
-			continue;
-		else
-			break;
-	}
-	return (0);
+        if (args != NULL && *args != NULL)
+        {
+            if (_strcmp(args[0], "exit") == 0)
+            {
+                if (args[1] != NULL)
+                {
+                    int exit_status = _atoi(args[1]);
+                    free(line);
+                    free_args(args);
+                    exit(exit_status);
+                }
+                else
+                {
+                    free(line);
+                    free_args(args);
+                    exit(EXIT_SUCCESS);
+                }
+            }
+
+            status = execute(args);
+            free_args(args);
+        }
+
+        free(line);
+        line = NULL;
+
+        if (status == EXIT_SUCCESS)
+        {
+            continue;
+        }
+    }
+
+    return (0);
 }
